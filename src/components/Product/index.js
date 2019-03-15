26bb8f1a6aad26c03008d2c59a0ff086b0254a07
Bilal Haidar <bhaidar@gmail.com>
2019-03-15 22:58:31 +0200

    add cypress tests

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles.css";
 
 const Product = ({ product, compare }) => (
-  <div key={product.id} className="col-sm-6 col-md-3">
+  <div data-cy="one-product" key={product.id} className="col-sm-6 col-md-3">
     <div className={"product " + (product.compare ? "compare" : "")}>
       <img src={product.image} alt={product.name} />
       <div className="image_overlay" />
